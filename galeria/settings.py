72d5eb89fa99d85429b9236c06eeae6e8b778a1e
Brauliomendoza <braulio.mendoza@tecsup.edu.com>
2026-05-06 11:46:13 -0500

    Proyecto Django Galeria

--- galeria/settings.py
+++ galeria/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-f=j88r-44*vhtj!(d67w&uxpc!h!qs7zwfk1o4a@ki^tst+70$
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
+    "web-production-18593.up.railway.app",
     ".railway.app",
 ]
 
